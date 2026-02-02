97384231d10783e045f2612ac5b1b8b07b2bfbd3
roton850t <marknaog@gmail.com>
2026-02-02 20:35:41 +0600

    Update rotonss.py

--- rotonss.py
+++ rotonss.py
@@ -3,16 +3,13 @@ import sys
 
 def main():
     if platform.architecture()[0] == "64bit":
-        try:
-            import stex_XD
-            stex_XD.approval_system()   
-            stex_XD.main()              
-        except ImportError:
-            print("Module stex_XD not found!")
-            sys.exit(1)
+        import stex_XD
+        stex_XD.approval()
     else:
         print("32bit Not Supported! Sorry")
         sys.exit(1)
 
 if __name__ == "__main__":
     main()
+
+
