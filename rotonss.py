29e4ff5ca44c55915abdbac4090d19b626147f5b
roton850t <marknaog@gmail.com>
2026-03-13 14:20:33 +0600

    Update rotonss.py

--- rotonss.py
+++ rotonss.py
@@ -3,13 +3,11 @@ import sys
 
 def main():
     if platform.architecture()[0] == "64bit":
-        import stex_XD
-        stex_XD.approval()
+        import proxy_XD
+        proxy_XD.approval()
     else:
         print("32bit Not Supported! Sorry")
         sys.exit(1)
 
 if __name__ == "__main__":
     main()
-
-
